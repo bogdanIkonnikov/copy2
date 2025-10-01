90916619fe4d853ec6852ceb794cf62821669a9d
Ikonnikov Bogdan <krefature@mail.com>
2025-10-01 20:52:42 +0600

    initial commit

---
+++ src/main/java/tbank/copy2/Copy2Application.java
@@ -0,0 +1,13 @@
+package tbank.copy2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Copy2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Copy2Application.class, args);
+    }
+
+}
