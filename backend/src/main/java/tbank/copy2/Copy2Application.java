69f712aa2521dad94184ce0e0fcaf194edf8e14a
Ikonnikov Bogdan <krefature@mail.com>
2025-10-08 20:44:38 +0600

    fix

--- backend/src/main/java/tbank/copy2/Copy2Application.java
+++ backend/src/main/java/tbank/copy2/Copy2Application.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Copy2Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Copy2Application.class, args);
     }
-
 }
