edc9c54afaa2f024492f716fd4352fa3e92b87c7
Ikonnikov Bogdan <krefature@mail.com>
2025-10-28 10:20:19 +0600

    feat(api): automatic api documentation

--- backend/src/main/java/tbank/copy2/controller/TestController.java
+++ backend/src/main/java/tbank/copy2/controller/TestController.java
@@ -1,23 +1,40 @@
 package tbank.copy2.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import tbank.copy2.dto.test.AddTestRequest;
+import tbank.copy2.dto.test.FullTestResponse;
 import tbank.copy2.dto.test.TestResponse;
 import tbank.copy2.service.TestService;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/modules")
 public class TestController {
 
     @Autowired
     private TestService testService;
 
-    @GetMapping("/modules")
+    @Operation(summary = "Получить список тестов")
+    @GetMapping()
     public List<TestResponse> getTests() {
         return testService.getTests();
     }
+
+    @Operation(summary = "Добавить новый тест")
+    @PostMapping("/add")
+    public void addTest(@RequestBody @Valid AddTestRequest request) {
+        testService.addTest(request);
+    }
+
+    @Operation(summary = "Получить тест по его id")
+    @GetMapping("/{id}")
+    public FullTestResponse getTestById(@PathVariable Long id) {
+        return testService.getTestById(id);
+    }
+
 }
+
--- backend/src/main/java/tbank/copy2/dto/test/TestResponse.java
+++ backend/src/main/java/tbank/copy2/dto/test/TestResponse.java
@@ -1,10 +1,31 @@
 package tbank.copy2.dto.test;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import tbank.copy2.entity.Test;
 
 @Data
 @AllArgsConstructor
+@Schema(description = "Ответ с информацией о тесте")
 public class TestResponse {
+
+    @Schema(description = "Название теста", example = "test1")
     private String name;
+
+    @Schema(description = "Описание теста", example = "Simple test, that contains 10 questions")
+    private String description;
+
+    @Schema(description = "Количество вопросов в тесте", example = "10")
+    private int questionsCount;
+
+    @Schema(description = "Прогресс прохождения теста", example = "14")
+    private int progress;
+
+    public TestResponse(Test test) {
+        this.name = test.getName();
+        this.description = test.getDescription();
+        this.questionsCount = test.getQuestions().size();
+        this.progress = 14; // Заглушка, заменить логикой
+    }
 }
 
