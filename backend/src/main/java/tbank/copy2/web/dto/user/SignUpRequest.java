0b43b9f3f0735c6f2d7bdbf2504e3b097395fe3e
Ikonnikov Bogdan <krefature@mail.com>
2026-02-26 12:21:51 +0600

    fix: set minimum length = 2 for username

--- backend/src/main/java/tbank/copy2/web/dto/user/SignUpRequest.java
+++ backend/src/main/java/tbank/copy2/web/dto/user/SignUpRequest.java
@@ -11,7 +11,7 @@ import lombok.Data;
 public class SignUpRequest {
 
     @Schema(description = "Имя пользователя", example = "Jon")
-    @Size(min = 5, max = 50, message = "Имя пользователя должно содержать от 5 до 50 символов")
+    @Size(min = 2, max = 50, message = "Имя пользователя должно содержать от 2 до 50 символов")
     @NotBlank(message = "Имя пользователя не может быть пустыми")
     private String username;
 
