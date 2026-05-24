7ed2b5c00a50bb406d8825beb3fb2517e0b1c4a4
Ikonnikov Bogdan <krefature@mail.com>
2026-05-24 17:50:21 +0600

    fix: save activityLogs after edit test

--- backend/src/main/java/tbank/copy2/infrastructure/persistence/entity/Test.java
+++ backend/src/main/java/tbank/copy2/infrastructure/persistence/entity/Test.java
@@ -42,7 +42,7 @@ public class Test {
     @OneToMany(mappedBy = "test", fetch = FetchType.LAZY, cascade = {CascadeType.MERGE, CascadeType.REFRESH}, orphanRemoval = true)
     private List<Question> questions = new ArrayList<>();
 
-    @OneToMany(mappedBy = "test", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    @OneToMany(mappedBy = "test", cascade = CascadeType.REMOVE)
     private List<ActivityLog> activityLogs = new ArrayList<>();
 
     @Column(nullable = false)
